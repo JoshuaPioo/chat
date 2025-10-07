3c9dbf9ba01057dd2cdf3219b251ea30e7f04515
JoshuaPioo <joshuatablon92@gmail.com>
2025-10-07 23:23:39 +0800

    Messages routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,18 @@
 import express from "express";
-
+import { getAllcontacts, getMessageByUserId, sendMessage, getChatPartners } from "../controllers/message.controller.js";
+import {protectRoute} from "../middleware/auth.middleware.js";
 const router = express.Router();
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
-router.get("/send", (req, res) => {
-    res.send("Send message endpoint");
-});
+
+//protect all routes after this middleware
+//this is done to avoid repeating the same middleware in all routes
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", getAllcontacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessageByUserId);
+router.post("/send/:id", sendMessage);
 
 
 export default router;
