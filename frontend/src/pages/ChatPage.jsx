b1c132d36c867032c2ad3555e14425c5e648fb56
JoshuaPioo <joshuatablon92@gmail.com>
2025-10-12 22:53:31 +0800

    login-page frontend

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore.js'
 
 function ChatPage() {
+
+  const {logout} = useAuthStore()
+
   return (
-    <div>ChatPage</div>
+    <div className='z-10 text-white'>ChatPage
+    <button onClick={logout}>Logout</button>
+    </div>
   )
 }
+
 
 export default ChatPage
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set) => ({
    authUser: null,
    isCheckingAuth: true,
    isSigningUp: false,
+   isLoggingIn: false,
 
    checkAuth : async () => {
     try {
@@ -32,6 +33,33 @@ export const useAuthStore = create((set) => ({
             
         }finally {
             set({isSigningUp: false})
+        }
+    
+},
+
+    login: async (data) => {
+        set({isLoggingIn: true})
+
+        try {
+         const res = await axiosInstance.post("/auth/login", data)
+            set({authUser: res.data})
+            
+            toast.success("Login successful!") 
+        } catch (error) {
+            toast.error(error.response.data.message)
+            
+        }finally {
+            set({isLoggingIn: false})
+        }
+    },
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout")
+            set({authUser: null})
+            toast.success("Logout successful!")
+        } catch (error) {
+            toast.error("Logout failed. Please try again.")
+            console.log("Logout error:", error)
         }
     }
 }));
