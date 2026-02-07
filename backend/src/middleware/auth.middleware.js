e515f6c82fdab76bff8d816bdd5266e39bbbbfed
JoshuaPioo <joshuatablon92@gmail.com>
2026-02-08 04:07:45 +0800

    Fix protectRoute decoded.userId

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,23 +2,30 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import { ENV } from "../lib/env.js";
 
-
 export const protectRoute = async (req, res, next) => {
-    try {
-        const token = req.cookies.jwt
-        if (!token)
-            return res.status(401).json({message: "Not authorized, no token"});
+  try {
+    const token = req.cookies?.jwt;
 
-        const decoded = jwt.verify(token, ENV.JWT_SECRET);
-        if (!decoded) return res.status(401).json({message: "Not authorized, token failed"});
-
-        const user = await User.findById(decoded.userID).select("-password");
-        if (!user) return res.status(404).json({message: "Not authorized, user not found"});
-
-        req.user = user;
-        next()
-    } catch (error) {
-        console.log("Error in protected route middleware", error.message);
-        res.status(500).json({message: "internal server error"});
+    if (!token) {
+      return res.status(401).json({ message: "Not authorized, no token" });
     }
-}
+
+    const decoded = jwt.verify(token, ENV.JWT_SECRET);
+    if (!decoded?.userId) {
+      return res.status(401).json({ message: "Not authorized, token failed" });
+    }
+
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Not authorized, user not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("Error in protected route middleware:", error.message);
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
